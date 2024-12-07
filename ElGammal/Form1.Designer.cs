c9437d88c17e2c1e78f16a5cf96e48c623aeaec3
Wscxw Gswef <d3midovk@gmail.com>
2024-12-07 05:21:18 +0300

    Добавьте файлы проекта.

---
+++ ElGammal/Form1.Designer.cs
@@ -0,0 +1,201 @@
+﻿namespace ElGammal
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelP = new System.Windows.Forms.Label();
+            this.textBoxP = new System.Windows.Forms.TextBox();
+            this.labelX = new System.Windows.Forms.Label();
+            this.textBoxX = new System.Windows.Forms.TextBox();
+            this.labelK = new System.Windows.Forms.Label();
+            this.textBoxK = new System.Windows.Forms.TextBox();
+            this.encryptButton = new System.Windows.Forms.Button();
+            this.saveButton = new System.Windows.Forms.Button();
+            this.loadButton = new System.Windows.Forms.Button();
+            this.resultLabel = new System.Windows.Forms.Label();
+            this.textBoxOutput = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxInput = new System.Windows.Forms.TextBox();
+            this.decryptButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // labelP
+            // 
+            this.labelP.Location = new System.Drawing.Point(30, 30);
+            this.labelP.Name = "labelP";
+            this.labelP.Size = new System.Drawing.Size(64, 28);
+            this.labelP.TabIndex = 0;
+            this.labelP.Text = "Число p:\r\nпростое";
+            // 
+            // textBoxP
+            // 
+            this.textBoxP.Location = new System.Drawing.Point(100, 30);
+            this.textBoxP.Name = "textBoxP";
+            this.textBoxP.Size = new System.Drawing.Size(100, 20);
+            this.textBoxP.TabIndex = 1;
+            // 
+            // labelX
+            // 
+            this.labelX.Location = new System.Drawing.Point(30, 70);
+            this.labelX.Name = "labelX";
+            this.labelX.Size = new System.Drawing.Size(64, 28);
+            this.labelX.TabIndex = 2;
+            this.labelX.Text = "Число x:\r\n1 < x < p";
+            // 
+            // textBoxX
+            // 
+            this.textBoxX.Location = new System.Drawing.Point(100, 70);
+            this.textBoxX.Name = "textBoxX";
+            this.textBoxX.Size = new System.Drawing.Size(100, 20);
+            this.textBoxX.TabIndex = 3;
+            // 
+            // labelK
+            // 
+            this.labelK.Location = new System.Drawing.Point(30, 110);
+            this.labelK.Name = "labelK";
+            this.labelK.Size = new System.Drawing.Size(64, 37);
+            this.labelK.TabIndex = 4;
+            this.labelK.Text = "Число k:\r\n1 < k < p-1\r\n";
+            // 
+            // textBoxK
+            // 
+            this.textBoxK.Location = new System.Drawing.Point(100, 110);
+            this.textBoxK.Name = "textBoxK";
+            this.textBoxK.Size = new System.Drawing.Size(100, 20);
+            this.textBoxK.TabIndex = 5;
+            // 
+            // encryptButton
+            // 
+            this.encryptButton.Location = new System.Drawing.Point(30, 150);
+            this.encryptButton.Name = "encryptButton";
+            this.encryptButton.Size = new System.Drawing.Size(75, 23);
+            this.encryptButton.TabIndex = 6;
+            this.encryptButton.Text = "Шифровать";
+            this.encryptButton.Click += new System.EventHandler(this.encryptButton_Click);
+            // 
+            // saveButton
+            // 
+            this.saveButton.Location = new System.Drawing.Point(599, 150);
+            this.saveButton.Name = "saveButton";
+            this.saveButton.Size = new System.Drawing.Size(75, 23);
+            this.saveButton.TabIndex = 7;
+            this.saveButton.Text = "Сохранить в файл";
+            this.saveButton.Click += new System.EventHandler(this.saveButton_Click);
+            // 
+            // loadButton
+            // 
+            this.loadButton.Location = new System.Drawing.Point(695, 150);
+            this.loadButton.Name = "loadButton";
+            this.loadButton.Size = new System.Drawing.Size(75, 23);
+            this.loadButton.TabIndex = 8;
+            this.loadButton.Text = "Загрузить из файла";
+            this.loadButton.Click += new System.EventHandler(this.loadButton_Click);
+            // 
+            // resultLabel
+            // 
+            this.resultLabel.Location = new System.Drawing.Point(30, 190);
+            this.resultLabel.Name = "resultLabel";
+            this.resultLabel.Size = new System.Drawing.Size(143, 23);
+            this.resultLabel.TabIndex = 9;
+            this.resultLabel.Text = "Результат шифрования:";
+            // 
+            // textBoxOutput
+            // 
+            this.textBoxOutput.Location = new System.Drawing.Point(30, 220);
+            this.textBoxOutput.Multiline = true;
+            this.textBoxOutput.Name = "textBoxOutput";
+            this.textBoxOutput.Size = new System.Drawing.Size(740, 180);
+            this.textBoxOutput.TabIndex = 10;
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(213, 4);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(113, 23);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "Текст шифрования:";
+            // 
+            // textBoxInput
+            // 
+            this.textBoxInput.Location = new System.Drawing.Point(216, 30);
+            this.textBoxInput.Multiline = true;
+            this.textBoxInput.Name = "textBoxInput";
+            this.textBoxInput.Size = new System.Drawing.Size(572, 103);
+            this.textBoxInput.TabIndex = 12;
+            // 
+            // decryptButton
+            // 
+            this.decryptButton.Location = new System.Drawing.Point(125, 150);
+            this.decryptButton.Name = "decryptButton";
+            this.decryptButton.Size = new System.Drawing.Size(224, 23);
+            this.decryptButton.TabIndex = 13;
+            this.decryptButton.Text = "Расшифровать из текста шифрования";
+            this.decryptButton.Click += new System.EventHandler(this.decryptButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.decryptButton);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxInput);
+            this.Controls.Add(this.labelP);
+            this.Controls.Add(this.textBoxP);
+            this.Controls.Add(this.labelX);
+            this.Controls.Add(this.textBoxX);
+            this.Controls.Add(this.labelK);
+            this.Controls.Add(this.textBoxK);
+            this.Controls.Add(this.encryptButton);
+            this.Controls.Add(this.saveButton);
+            this.Controls.Add(this.loadButton);
+            this.Controls.Add(this.resultLabel);
+            this.Controls.Add(this.textBoxOutput);
+            this.Name = "Form1";
+            this.Text = "Шифрование Эль-Гаммаля";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        // Элементы управления
+        private System.Windows.Forms.Label labelP;
+        private System.Windows.Forms.TextBox textBoxP;
+
+        private System.Windows.Forms.Label labelX;
+        private System.Windows.Forms.TextBox textBoxX;
+
+        private System.Windows.Forms.Label labelK;
+        private System.Windows.Forms.TextBox textBoxK;
+
+        private System.Windows.Forms.Button encryptButton;
+        private System.Windows.Forms.Button saveButton;
+        private System.Windows.Forms.Button loadButton;
+
+        private System.Windows.Forms.Label resultLabel;
+        private System.Windows.Forms.TextBox textBoxOutput;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxInput;
+        private System.Windows.Forms.Button decryptButton;
+    }
+}
---
+++ ElGammal/Form1.cs
@@ -0,0 +1,312 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Numerics;
+using System.Security.Cryptography;
+using System.Reflection.Emit;
+using System.Text.RegularExpressions;
+using System.IO;
+
+namespace ElGammal
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void encryptButton_Click(object sender, EventArgs e)
+        {
+            // Проверка, чтобы в textBoxP, textBoxX, и textBoxK находились числа
+            if (!BigInteger.TryParse(textBoxP.Text, out BigInteger p) || p < 997)
+            {
+                MessageBox.Show("В поле P должно быть указано простое число больше 997.");
+                return;
+            }
+
+            if (!BigInteger.TryParse(textBoxX.Text, out BigInteger x) || x <= 1 || x >= p)
+            {
+                MessageBox.Show("В поле X должно быть указано число в пределах от 1 до P.");
+                return;
+            }
+
+            if (!BigInteger.TryParse(textBoxK.Text, out BigInteger k) || k <= 1 || k >= p - 1)
+            {
+                MessageBox.Show("В поле K должно быть указано число в пределах от 1 до P-1.");
+                return;
+            }
+
+            // Генерация ключей
+            var (pGenerated, g, xGenerated, y) = GenerateKeys();
+            if (pGenerated == BigInteger.Zero) return; // Если возникла ошибка при генерации ключа
+
+            // Считывание сессионного ключа из поля (k уже проверен выше)
+            // Преиспользуем значение p, g, x, y для ключей, если они успешно сгенерированы
+            p = pGenerated;
+            x = xGenerated;
+
+            // Шифруем сообщение
+            string message = textBoxInput.Text;
+            var cipherText = EncryptMessage(message, p, g, y, k);
+
+            // Вывод зашифрованного текста
+            StringBuilder encryptedMessage = new StringBuilder();
+            foreach (var (a, b) in cipherText)
+            {
+                //encryptedMessage.Append($"({a}, {b}) ");
+                encryptedMessage.Append($"({a}, {b}) ");
+            }
+
+            textBoxOutput.Text = encryptedMessage.ToString();
+        }
+
+
+        // Функция для вычисления степени по модулю (g^k mod p)
+        private BigInteger ModPow(BigInteger baseValue, BigInteger exponent, BigInteger modulus)
+        {
+            return BigInteger.ModPow(baseValue, exponent, modulus);
+        }
+
+        // Функция для нахождения первообразного корня с использованием функции Эйлера
+        private BigInteger FindPrimitiveRoot(BigInteger p)
+        {
+            BigInteger phi = p;  // Функция Эйлера для простого числа p
+            List<BigInteger> divisors = GetDivisors(phi);  // Получаем делители phi(p)
+
+            for (BigInteger g = 2; g < p; g++)
+            {
+                bool isPrimitiveRoot = true;
+
+                // Проверяем, является ли g первообразным корнем
+                foreach (BigInteger divisor in divisors)
+                {
+                    // Проверяем, что g^((phi(p))/divisor) % p != 1
+                    if (ModPow(g, phi / divisor, p) == 1)
+                    {
+                        isPrimitiveRoot = false;
+                        break;
+                    }
+                }
+
+                // Если не нашли нарушений, g является первообразным корнем
+                if (isPrimitiveRoot)
+                {
+                    return g;
+                }
+            }
+
+            // Если не найдено, возвращаем -1 (ошибка)
+            return -1;
+        }
+
+
+        // Функция для нахождения делителей числа n
+        private List<BigInteger> GetDivisors(BigInteger n)
+        {
+            List<BigInteger> divisors = new List<BigInteger>();
+            for (BigInteger i = 1; i <= n; i++)
+            {
+                if (n % i == 0)
+                {
+                    divisors.Add(i);
+                }
+            }
+            return divisors;
+        }
+
+        // Генерация открытого и закрытого ключа
+        private (BigInteger p, BigInteger g, BigInteger x, BigInteger y) GenerateKeys()
+        {
+            BigInteger p = BigInteger.Parse(textBoxP.Text);  // Простое число p
+
+            // Ищем первообразный корень g
+            BigInteger g = FindPrimitiveRoot(p);
+            if (g == -1)
+            {
+                MessageBox.Show("Не удалось найти первообразный корень для числа p.");
+                return (BigInteger.Zero, BigInteger.Zero, BigInteger.Zero, BigInteger.Zero);
+            }
+
+            BigInteger x = BigInteger.Parse(textBoxX.Text);  // Закрытый ключ x
+
+            // Открытый ключ y = (g^x) % p
+            BigInteger y = ModPow(g, x, p);
+
+            return (p, g, x, y);
+        }
+
+        // Шифрование сообщения по алгоритму Эль-Гамаля
+        private List<Tuple<BigInteger, BigInteger>> EncryptMessage(string message, BigInteger p, BigInteger g, BigInteger y, BigInteger k)
+        {
+            List<Tuple<BigInteger, BigInteger>> cipherText = new List<Tuple<BigInteger, BigInteger>>();
+
+            foreach (char c in message)
+            {
+                BigInteger M = (BigInteger)c; // Преобразуем символ в число (ASCII код)
+                BigInteger a = ModPow(g, k, p);  // a = g^k mod p
+                BigInteger b = (ModPow(y, k, p) * M) % p;  // b = (y^k * M) mod p
+
+                cipherText.Add(new Tuple<BigInteger, BigInteger>(a, b));
+            }
+
+            return cipherText;
+        }
+
+        private void saveButton_Click(object sender, EventArgs e)
+        {
+            // Открытие диалога выбора файла
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "Text Files (*.txt)|*.txt|All Files (*.*)|*.*"; // Фильтр для типов файлов
+            saveFileDialog.Title = "Сохранить файл";
+
+            // Если пользователь выбрал файл и нажал "Сохранить"
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                // Получаем путь к файлу
+                string filePath = saveFileDialog.FileName;
+
+                try
+                {
+                    // Сохраняем текст из textBoxOutput в выбранный файл
+                    System.IO.File.WriteAllText(filePath, textBoxOutput.Text);
+                    MessageBox.Show("Файл успешно сохранен!", "Успех", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                catch (Exception ex)
+                {
+                    // В случае ошибки выводим сообщение
+                    MessageBox.Show($"Ошибка при сохранении файла: {ex.Message}", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        private void loadButton_Click(object sender, EventArgs e)
+        {
+            // Создаем диалог выбора файла
+            OpenFileDialog openFileDialog = new OpenFileDialog
+            {
+                Filter = "Text Files (*.txt)|*.txt|All Files (*.*)|*.*",
+                Title = "Выберите текстовый файл"
+            };
+
+            // Показываем диалог и проверяем, выбрал ли пользователь файл
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    // Читаем содержимое файла
+                    string fileContent = File.ReadAllText(openFileDialog.FileName);
+
+                    // Загружаем текст в текстовое поле
+                    textBoxInput.Text = fileContent;
+                }
+                catch (Exception ex)
+                {
+                    // Обрабатываем возможные ошибки
+                    MessageBox.Show($"Ошибка при чтении файла: {ex.Message}", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+
+        private void decryptButton_Click(object sender, EventArgs e)
+        {
+            // Проверка, чтобы в textBoxP и textBoxX находились числа
+            if (!BigInteger.TryParse(textBoxP.Text, out BigInteger p) || p <= 1)
+            {
+                MessageBox.Show("В поле P должно быть указано простое число больше 1.");
+                return;
+            }
+
+            if (!BigInteger.TryParse(textBoxX.Text, out BigInteger x) || x <= 1 || x >= p)
+            {
+                MessageBox.Show("В поле X должно быть указано число в пределах от 1 до P.");
+                return;
+            }
+
+            // Считываем зашифрованное сообщение
+            string encryptedText = textBoxInput.Text.Trim();
+            if (string.IsNullOrWhiteSpace(encryptedText))
+            {
+                MessageBox.Show("Поле зашифрованного текста не должно быть пустым.");
+                return;
+            }
+
+            // Разбираем зашифрованное сообщение в список пар (a, b)
+            var cipherText = new List<Tuple<BigInteger, BigInteger>>();
+            try
+            {
+                var matches = Regex.Matches(encryptedText, @"\((\d+),\s*(\d+)\)");
+                foreach (Match match in matches)
+                {
+                    if (BigInteger.TryParse(match.Groups[1].Value, out BigInteger a) &&
+                        BigInteger.TryParse(match.Groups[2].Value, out BigInteger b))
+                    {
+                        cipherText.Add(new Tuple<BigInteger, BigInteger>(a, b));
+                    }
+                    else
+                    {
+                        throw new Exception("Неверный формат зашифрованного текста.");
+                    }
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Неверный формат зашифрованного текста. Убедитесь, что он состоит из пар чисел в формате (a, b).");
+                return;
+            }
+
+            // Расшифровываем сообщение
+            string decryptedMessage = DecryptMessage(cipherText, p, x);
+
+            // Вывод расшифрованного текста
+            textBoxOutput.Text = decryptedMessage;
+        }
+
+
+        // Функция для расшифрования сообщения по алгоритму Эль-Гамаля
+        private string DecryptMessage(List<Tuple<BigInteger, BigInteger>> cipherText, BigInteger p, BigInteger x)
+        {
+            StringBuilder decryptedMessage = new StringBuilder();
+
+            foreach (var (a, b) in cipherText)
+            {
+                // Вычисляем a^x mod p
+                BigInteger ax = ModPow(a, x, p);
+
+                // Вычисляем мультипликативную инверсию a^x mod p
+                BigInteger axInverse = ModInverse(ax, p);
+
+                // Восстанавливаем исходное сообщение M = (b * axInverse) % p
+                BigInteger M = (b * axInverse) % p;
+                if (M < 0 || M > char.MaxValue)
+                {
+                    throw new Exception($"Расшифрованное значение '{M}' выходит за пределы диапазона символов.");
+                }
+                decryptedMessage.Append((char)M);
+
+
+            }
+
+            return decryptedMessage.ToString();
+        }
+
+        // Функция для нахождения мультипликативной инверсии
+        private BigInteger ModInverse(BigInteger a, BigInteger p)
+        {
+            return BigInteger.ModPow(a, p - 2, p); // Используем теорему Эйлера для вычисления обратного элемента
+        }
+
+
+
+
+    }
+}
+
+
